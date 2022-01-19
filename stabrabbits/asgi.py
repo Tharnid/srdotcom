ddd10cb1f95a79e05699313818309cf3fed9b83e
Tharnid <jeffmsmithdev@gmail.com>
2022-01-18 23:06:37 -0500

    initial commit

---
+++ stabrabbits/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for stabrabbits project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stabrabbits.settings')
+
+application = get_asgi_application()

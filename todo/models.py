cfd8e57dc338c88865ad775ed3f302cdfcc9387e
Tharnid <jeffmsmithdev@gmail.com>
2022-02-05 17:05:21 -0500

    done with todos

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    note = models.TextField(max_length=100, blank=True) # blank is equal to null
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True, blank=True) # null is blank for DateTimeField
+    modified = models.DateTimeField(auto_now=True)
+    important = models.BooleanField(default=False)
+    user =  models.ForeignKey(User, on_delete=models.CASCADE) # foreign key/relationship
+    
+    # display the todos better in admin
+
+    def __str__(self):
+        return self.title    
--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
+from .forms import TodoForm
+from .models import Todo
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 # home
+@login_required
 def home(request):
     return render(request, 'todo/home.html')
-
-
 # auth
 
 def signupuser(request):
@@ -38,12 +41,64 @@ def loginuser(request):
         else:
             login(request, user)
             return redirect('currenttodos')
-               
+
+@login_required               
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
     
-
+# todos
+@login_required
 def currenttodos(request):
-    return render(request, 'todo/currenttodos.html')
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True).order_by('-created')
+    return render(request, 'todo/currenttodos.html', {'todos': todos})
+
+@login_required
+def completedtodos(request):
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'todo/completedtodos.html', {'todos': todos})
+
+@login_required
+def viewtodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'GET':
+        form = TodoForm(instance=todo) # very cool it will automatically forward the info
+        return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form})
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            form = TodoForm(request.POST)     
+            return render(request, 'todo/viewtodo.html', {'todo':todo(), 'form': form,  'error': 'Bad Data Passed In.  Try Again!!!'})
+
+@login_required
+def createtodo(request):
+    if request.method == 'GET':
+        return render(request, 'todo/createtodo.html', {'form': TodoForm()})        
+    else:
+        try:
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False)
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/createtodo.html', {'form':TodoForm(), 'error': 'Bad Data Passed In.  Try Again!!!'})    
+
+@login_required        
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.datecompleted = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
+
+@login_required    
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodos')
